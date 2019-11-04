a2ed994959bc6cac05d52aa72ee9e4b857cfdc62
gk100799 <55948183+gk100799@users.noreply.github.com>
2019-11-04 17:25:46 +0530

    Add files via upload

--- ERS_app/models.py
+++ ERS_app/models.py
@@ -2,22 +2,21 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 
-# Create your models here.
+ 
 class ERSEvent(models.Model):
-    # id = models.CharField(max_length=4, unique = true, blank=False, null=False)
-    name = models.CharField(max_length=15, blank=False, null=False)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    #description = models.TextField(blank=True)
+    id = models.CharField(max_length=4, primary_key = True)
+    event_name = models.CharField(max_length=30, blank=True)
+    description = models.TextField(blank=True)
     organizers = models.CharField(max_length=15, blank=True)
-    location = models.CharField(max_length=20, blank=True)
+    venue = models.CharField(max_length=20, blank=True)
     time = models.TimeField()
+    date = models.DateField(blank = True)
     limit = models.IntegerField(blank=True)
     fee = models.CharField(max_length=10)
 
 class ERSQuestion(models.Model):
     event_id = models.ForeignKey(to=ERSEvent, on_delete=models.CASCADE)
-    questionType = models.CharField(max_length=10, default='MCQ')
+    #questionType = models.CharField(max_length=10, default='MCQ')
     question = models.TextField(max_length=75, blank=False)
     option1 = models.CharField(max_length=30, blank=True, null=True)
     option2 = models.CharField(max_length=30, blank=True, null=True)
--- ERS_app/views.py
+++ ERS_app/views.py
@@ -7,28 +7,34 @@ from django.contrib import messages
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in
 from django.contrib.auth.decorators import login_required
-
-
+from .models import (ERSEvent, ERSQuestion, ERSSurvey)
+from django.db import connection
 # Create your views here.
-def index(request):
-    # import ipdb; ipdb.set_trace()
-    return render(request, "addUser.html")
-
+# def index(request):
+#     # import ipdb; ipdb.set_trace()
+#     return render(request, "addUser.html")
+ 
 
     #@csrf_exempt
 def addUser(request):
     #import ipdb; ipdb.set_trace()
-    data = request.POST
-    username=data['username']
-    email=data['email']
-    password=data['password']
-    firstname=data['firstname']
-    lastname=data['lastname']
-    user = User.objects.create_user(username, email, password)
-    user.first_name = firstname
-    user.last_name = lastname
-    user.save()
-    return HttpResponse("Hurray!! You're registered!")
+    if request.method == 'POST':
+        data = request.POST 
+        username=data['username']
+        email=data['email']
+        password=data['password']
+        firstname=data['firstname']
+        lastname=data['lastname']
+        user = User.objects.create_user(username, email, password)
+        user.first_name = firstname
+        user.last_name = lastname
+        user.is_staff=True
+        user.save()
+        print(connection.queries)
+        return HttpResponse("Hurray!! You're registered!")
+    return render(request = request,
+                template_name = "addUser.html/")
+
 
     #adduser = ERSUser.objects.create(username=data['username'], password=data['password'], email=data['email'], firstname=data['firstname'], lastname=data['lastname'], branch=data['branch'], phone=data['phone'])
     #adduser.save()
@@ -42,11 +48,14 @@ def user_login(request):
         username = request.POST['username']
         password = request.POST['password']
         User= authenticate(request,username=username, password=password)
+        print(connection.queries)
         if User is not None:
             login(request, User)
-            return redirect('home/')
+            if not User.is_staff:
+                return redirect('home/')
+            return redirect('admin1/')
         else:
-             messages.error(request, "Invalid username or password.")
+             messages.error(request,"Invalid username or password.")
     return render(request = request,
                 template_name = "login.html/")
 
@@ -77,7 +86,60 @@ def user_login(request):
 
 @login_required(login_url='/')
 def homepage(request):
-    return render(request, "home.html")   
+    return render(request, "student.html")  
+
+@login_required(login_url='/')
+def adminpage(request):
+    return render(request, "admin.html")
+       
+@login_required(login_url='/')
+def gaandupage(request):
+    return render(request, "event-create (Final).html")
+
+@login_required(login_url='/')
+@csrf_exempt
+def createevent(request):
+    #import ipdb; ipdb.set_trace()
+    if request.method == 'POST':
+        data = request.POST
+        id1 = request.POST.get('event_id')
+        event_name = data.get('event_name')
+        description = data.get('description')
+        organizers = data.get('organizers')
+        venue = data.get('venue')
+        time = data.get('time')
+        limit = data.get('limit')
+        fee = data.get('fee')
+        date = data.get('date')
+        question = ERSEvent(id = id1, event_name = event_name, description = description, organizers = organizers, venue = venue, time = time, limit = limit, fee = fee, date = date)
+        question.save()
+        return HttpResponse("<h1 style='color:red'>Hurray!! Event created</h1>")
+    #return render(request, "event-create (Final).html")
+    return render(request = request,
+                template_name = "event-create (Final).html/")   
+
+
+@login_required(login_url='/')
+@csrf_exempt
+def modifyevent(request):
+    #import ipdb; ipdb.set_trace()
+    if request.method == 'POST':
+        data = request.POST
+        id1 = request.POST.get('event_id')
+        event_name = data.get('event_name')
+        description = data.get('description')
+        organizers = data.get('organizers')
+        venue = data.get('venue')
+        time = data.get('time')
+        limit = data.get('limit')
+        fee = data.get('fee')
+        date = data.get('date')
+        question = ERSEvent(id = id1, event_name = event_name, description = description, organizers = organizers, venue = venue, time = time, limit = limit, fee = fee, date = date)
+        question.save()
+        return HttpResponse("Hurray!! Event edited")
+    #return render(request, "event-create (Final).html")
+    return render(request = request,
+                template_name = "modify.html/") 
 
 
 
